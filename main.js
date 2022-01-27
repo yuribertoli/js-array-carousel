e67ae153a8b381f721468cfdb17d1400543921c1
yuribertoli <yuribertoli@yahoo.it>
2022-01-27 09:06:40 +0100

    click

--- main.js
+++ main.js
@@ -15,11 +15,11 @@ const title = [
 ]
 
 const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+    'Svezia Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Svizzera Lorem ipsum',
+    'Gran Bretagna Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Germania Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Paradise Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
 ]
 
 let immagini = "";
@@ -53,16 +53,65 @@ let overlay = document.getElementsByClassName("overlay");
 immagineAttiva[momento].classList.add("on");
 overlay[momento].classList.add("on");
 
+//Definisco la variabile click settandola a zero
+let click = 0;
+
 //Cambio momento in base al click 
-const arrowUp = document.getElementById("arrow-up");
+document.getElementById("arrow-down").addEventListener("click",
 
-arrowUp.addEventListener("click",
+    function () {
 
-    function() {
+        click = click + 1;
+        
+        if (click == items.length) {
+            click = 0;
+        }
 
-        immagineAttiva[momento].classList.remove("on");
-        immagineAttiva[momento + 1].classList.add("on");
+        if (click == 0) {
+
+            immagineAttiva[items.length - 1].classList.remove("on");
+            overlay[items.length - 1].classList.remove("on");
+
+        } else {
+
+            immagineAttiva[click - 1].classList.remove("on");
+            overlay[click - 1].classList.remove("on");
+
+        }
+
+        immagineAttiva[click].classList.add("on");
+        overlay[click].classList.add("on");
 
     }
 
-)
+);
+
+document.getElementById("arrow-up").addEventListener("click",
+
+    function() {
+
+        if (click == 0) {
+            click = items.length;
+        }
+
+        click = click - 1;
+
+        if (click == items.length - 1) {
+
+            immagineAttiva[0].classList.remove("on");
+            overlay[0].classList.remove("on");
+
+        } else {
+
+            immagineAttiva[click + 1].classList.remove("on");
+            overlay[click + 1].classList.remove("on");
+
+        }
+
+        immagineAttiva[click].classList.add("on");
+        overlay[click].classList.add("on");
+
+    }
+
+); 
+
